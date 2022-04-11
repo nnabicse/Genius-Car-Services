fe01eb934982fda079375d737b9d80f2170c5ee4
Nurun Nabi <nnabi.cse@gmail.com>
2022-04-11 18:06:22 +0600

    services responsive

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
-
+import './Services.css'
 const Services = () => {
     const [services, setServices] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('services.json')
-        .then(res=>res.json())
-        .then(data=>setServices(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
     return (
         <div>
-            <h2>Services: {services.length}</h2>
-            {
-                services.map(service => <Service
-                    key={service.key}
-                    service  = {service}
-                ></Service>)
-            }
+            <h1 className='services-title'>Our Services: {services.length}</h1>
+            <div className='services-container'>
+                {
+                    services.map(service => <Service
+                        key={service.key}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
