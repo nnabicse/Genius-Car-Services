1f3cdee48bf634f00e5cfb90dc571b3c0947d070
Nurun Nabi <nnabi.cse@gmail.com>
2022-04-11 17:50:18 +0600

    services loaded

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Service from './Service/Service';
 import Services from './Services/Services';
 
 const Home = () => {
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
 
 const Services = () => {
     const [services, setServices] = useState([]);
@@ -10,6 +11,12 @@ const Services = () => {
     return (
         <div>
             <h2>Services: {services.length}</h2>
+            {
+                services.map(service => <Service
+                    key={service.key}
+                    service  = {service}
+                ></Service>)
+            }
         </div>
     );
 };
