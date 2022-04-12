95b236eea5edca8532d9b5610250470760a2b6d3
Nurun Nabi <nnabi.cse@gmail.com>
2022-04-12 22:46:17 +0600

    set envt variables for firebase init

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,44 +3,41 @@ import { Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Register = () => {
-    const emailRef = useRef("");
-    const passwordRef = useRef("");
-    const confirmPasswordRef = useRef("");
     const navigate = useNavigate()
 
-    const handleSubmit = (event) => {
+    const handleRegister = (event) => {
         event.preventDefault();
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-        const confirmPassword = confirmPasswordRef.current.value;
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
     }
 
-    const navigateRegister = event => {
+    const navigateLogin = event => {
         navigate('/login')
     }
     return (
         <div>
             <div className='container w-50 mx-auto'>
                 <h2 className='text-primary text-center'>Please Register</h2>
-                <Form onSubmit={handleSubmit}>
+                <Form onSubmit={handleRegister}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control required type="name" placeholder="Enter name" />
+                    </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control required ref={emailRef} type="email" placeholder="Enter email" />
+                        <Form.Control required type="email" placeholder="Enter email" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control required ref={passwordRef} type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control required ref={confirmPasswordRef} type="password" placeholder="Confirm Password" />
+                        <Form.Control required type="password" placeholder="Password" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Check me out" />
                     </Form.Group>
                     <button className='btn btn-primary' type='submit'>Submit</button>
                 </Form>
-                <p>Already Have a Account? <Link to='/login' className='text-primary pe-auto text-decoration-none' onClick={navigateRegister}>Please Login</Link></p>
+                <p>Already Have a Account? <Link to='/login' className='text-primary pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
             </div>
         </div>
     );
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyADTDTIrckcUueuFqk9ll056FrCyJ_QEyY",
-    authDomain: "genius-car-services-cea1f.firebaseapp.com",
-    projectId: "genius-car-services-cea1f",
-    storageBucket: "genius-car-services-cea1f.appspot.com",
-    messagingSenderId: "915613135129",
-    appId: "1:915613135129:web:e987e41b7cc6b9d4225588"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
